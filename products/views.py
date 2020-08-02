e1969d70b0503d88bdd9bf35d181e2572dd528e6
aidant842 <aidant842@gmail.com>
2020-08-02 14:34:24 +0000

    Added pagination to products page, complete with stying. Added forgotten style for scrollbar thumb

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from.models import Product
+
 
 # Create your views here.
 
@@ -8,9 +10,20 @@ def all_products(request):
     """ A view to return the products page """
 
     products = Product.objects.all()
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
 
     context = {
-        'products': products
+        'products': products,
+        'page': page,
     }
 
     return render(request, 'products/products.html', context)
