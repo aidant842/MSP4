78e011f1743a6e3358f11c7639de9b7142c3f90a
aidant842 <aidant842@gmail.com>
2020-08-04 18:22:29 +0000

    Added filtering by category to products app, imported product model to home view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from.models import Product
+from.models import Product, Category
 
 
 # Create your views here.
@@ -10,8 +10,18 @@ def all_products(request):
     """ A view to return the products page """
 
     products = Product.objects.all()
+    categories = Category.objects.all()
     paginator = Paginator(products, 6)
     page = request.GET.get('page')
+
+    selected_category = None
+    filtered_products = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            selected_category = request.GET['category']
+            filtered_products = products.filter(category__name=selected_category)
+            print(filtered_products)
 
     try:
         products = paginator.page(page)
@@ -22,7 +32,10 @@ def all_products(request):
         products = paginator.page(paginator.num_pages)
 
     context = {
+        'filtered_products': filtered_products,
         'products': products,
+        'categories': categories,
+        'selected_category': selected_category,
         'page': page,
     }
 
