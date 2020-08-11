b88d15f474c94eb331967f916e942b5ff0a4a510
aidant842 <aidant842@gmail.com>
2020-08-11 15:06:59 +0000

    added add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-from products.models import Product
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ A view to return the bag contents page """
 
-    products = Product.objects.all()
+    return render(request, 'bag/bag.html')
 
-    context = {
-        'products': products,
-    }
 
-    return render(request, 'bag/bag.html', context)
+def add_to_bag(request, item_id):
+    """ Add a quantity of an item to the bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    size = request.POST.get('size')
+    colour = request.POST.get('colour')
+    material = request.POST.get('material')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
