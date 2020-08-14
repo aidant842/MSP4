f2356b630016b471f1a0d4a7f5189802ecbc6dc7
aidant842 <aidant842@gmail.com>
2020-08-14 17:39:27 +0000

    added a new app to store custom filters, registered display_price filter, to display correct price in price fields

--- bag/contexts.py
+++ bag/contexts.py
@@ -9,16 +9,26 @@ def bag_contents(request):
     delivery = total * .10
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product
-        })
-
-
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+            print(item_data)
     grand_total = total + delivery
 
     context = {
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
 
+
 def view_bag(request):
     """ A view to return the bag contents page """
 
@@ -13,15 +14,32 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = request.POST.get('size')
-    colour = request.POST.get('colour')
-    material = request.POST.get('material')
     bag = request.session.get('bag', {})
+    size = None
+    if 'size' in request.POST:
+        size = request.POST['size']
+    material = None
+    if 'material' in request.POST:
+        material = request.POST['material']
+    colour = None
+    if 'colour' in request.POST:
+        colour = request.POST['colour']
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
+
     return redirect(redirect_url)
